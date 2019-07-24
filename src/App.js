e23df2753481fe10e9b23843e747891cd69dfa5f
Yu Tong Chen <35983531+enterdoki@users.noreply.github.com>
2019-07-24 18:56:14 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+// import CitySearch from "./Components/CitySearch/CitySearch"
+import Zipcode from "./Components/Zipcode/Zipcode";
 import './App.css';
+import CitySearch from './Components/CitySearch/CitySearch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <div><h2>Zip Code and City Search</h2></div>
+            
+          </header>
+          <Zipcode/>
+          <CitySearch/>
+        </div>
+      );  
+  } 
 }
 
 export default App;
